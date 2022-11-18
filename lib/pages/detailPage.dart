92a69a2e142d75a522fbe7108d8519ce30d7cdf3
GandhiManan <114692191+GandhiManan@users.noreply.github.com>
2022-11-18 17:55:46 -0500

    v1

--- lib/pages/detailPage.dart
+++ lib/pages/detailPage.dart
@@ -12,41 +12,123 @@ class DetailPage extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Stack(
-          children: [
-            ListView(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(32),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 300,
-                      ),
-                      Text(
-                        planetInfo.name,
-                        style: TextStyle(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(height: 300),
+                        Text(
+                          planetInfo.name,
+                          style: TextStyle(
                             fontFamily: 'Avenir',
                             fontSize: 56,
                             color: primaryTextColor,
-                            fontWeight: FontWeight.w900),
-                        textAlign: TextAlign.left,
-                      ),
-                      Text(
-                        'Solar System',
-                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          'Solar System',
+                          style: TextStyle(
                             fontFamily: 'Avenir',
                             fontSize: 31,
                             color: primaryTextColor,
                             fontWeight: FontWeight.w300,
+                          ),
+                          textAlign: TextAlign.left,
                         ),
-                        textAlign: TextAlign.left,
-                      )
-                    ],
+                        const Divider(color: Colors.black38),
+                        const SizedBox(height: 32),
+                        Text(
+                          planetInfo.description ?? '',
+                          maxLines: 5,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontFamily: 'Avenir',
+                            fontSize: 20,
+                            color: contentTextColor,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(height: 32),
+                        const Divider(color: Colors.black38),
+                      ],
+                    ),
                   ),
-                )
-              ],
-            )
+                  const Padding(
+                    padding: EdgeInsets.only(left: 32.0),
+                    child: Text(
+                      'Gallery',
+                      style: TextStyle(
+                        fontFamily: 'Avenir',
+                        fontSize: 25,
+                        color: Color(0xff47455f),
+                        fontWeight: FontWeight.w300,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Container(
+                    height: 250,
+                    padding: const EdgeInsets.only(left: 32.0),
+                    child: ListView.builder(
+                      itemCount: planetInfo.images.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.network(
+                              planetInfo.images[index],
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: -64,
+              child: Hero(
+                  tag: planetInfo.positon,
+                  child: Image.asset(planetInfo.iconImage)),
+            ),
+            Positioned(
+              top: 60,
+              left: 32,
+              child: Text(
+                planetInfo.positon.toString(),
+                style: TextStyle(
+                  fontFamily: 'Avenir',
+                  fontSize: 247,
+                  color: primaryTextColor.withOpacity(0.08),
+                  fontWeight: FontWeight.w900,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0,top: 12.0),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
           ],
         ),
       ),
